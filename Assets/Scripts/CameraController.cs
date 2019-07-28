f3712926b8a146e043217a6a3b241d38112ad5b0
andrew <andrew.d.rose.i@gmail.com>
2019-07-28 09:39:50 -0700

    Added Backgrounds. Added parallax scrolling. Updated camera smoothing.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public Transform target;            // Target is what the camera will follow
-    public float smoothSpeed;           // Speed of the camera when moving
-    public Vector3 offset;
-    public Vector2 maxPosition;
-    public Vector2 minPosition;
+    public Transform target;                                                        // The point that the camera will follow.
+    [SerializeField] bool smoothing = false;                                        // You can turn camera smoothing ON/OFF.
+    [SerializeField] private float smoothSpeed = 5f;                                // Speed of the camera when moving.
+    [SerializeField] private float focusPointPercentBetweenTargetAndMouse = 0.25f;  /* Focus the camera on a point between the Mouse and Target by
+                                                                                       the specified amount.
+                                                                                       Example: 0.25f would focus the camera 25% of the distance
+                                                                                                between the Mouse and Target.
+                                                                                    */
 
-    private void LateUpdate()
+    /* Smoothing the camera in LateUpdate causes a jittery effect to the player sprite.
+       FixedUpdate removes any jitters for some reason.
+    */
+    private void FixedUpdate()
     {
-        if(target != null && transform.position != target.position)
+        if(target != null)
         {
-            Vector3 desiredPosition = target.position + offset;
-            //desiredPosition.x = Mathf.Clamp(desiredPosition.x, minPosition.x, maxPosition.x);
-            //desiredPosition.y = Mathf.Clamp(desiredPosition.y, minPosition.y, maxPosition.y);
-            transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            Vector3 mousePosToWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 distBetweenMouseAndTarget = mousePosToWorld - target.position;
+            Vector3 desiredPosition = (focusPointPercentBetweenTargetAndMouse * distBetweenMouseAndTarget) + target.position;
+            if (smoothing)
+            {
+                Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+                transform.position = smoothedPosition;
+            }else
+                transform.position = desiredPosition;
         }
     }
 }
---
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour {
+
+    private float length;
+    private float startPos;
+    [SerializeField] private float parallaxEffect;
+    public GameObject cam;
+
+	void Start () {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+	}
+	
+	void FixedUpdate () {
+        float temp = cam.transform.position.x * (1 - parallaxEffect);
+        float dist = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPos + length)
+            startPos += length;
+        else if (temp < startPos - length)
+            startPos -= length;
+	}
+}
